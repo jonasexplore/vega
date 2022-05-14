c871e595cb186a8c5e210b8c5b811af3cf44d96c
Jonas <jonasexplore@gmail.com>
2022-05-14 12:39:43 -0300

    feat(components): added new fields and mapped form

--- web/src/components/Form.tsx
+++ web/src/components/Form.tsx
@@ -1,21 +1,52 @@
-import { Button, Form, Input } from "antd";
+import { Button, Checkbox, Form, Input, Slider } from "antd";
+
+type Props = {
+  onFinish: (props: any) => void;
+};
 
 const { Item } = Form;
 
-export const SearchForm = () => (
-  <Form>
-    <Item>
-      <Input placeholder="Armazenamento" />
+export const SearchForm = ({ onFinish }: Props) => (
+  <Form
+    style={{
+      width: "30%",
+    }}
+    onFinish={onFinish}
+  >
+    <Item label="Memória interna (GB)" name="int_memory">
+      <Slider min={16} max={512} step={16} />
     </Item>
-    <Item>
-      <Input placeholder="Memória RAM" />
+    <Item label="Quantidade de RAM (GB)" name="ram">
+      <Slider min={1} max={16} />
     </Item>
-    <Item>
-      <Input placeholder="Número de Núcleos" />
+    <Item label="Nº de núcleos" name="n_cores">
+      <Slider min={1} max={8} />
     </Item>
-    <Item>
-      <Input placeholder="Velocidade do Processador" />
+    <Item label="Clock do processador (Ghz)" name="clock_speed">
+      <Slider min={0.1} max={4} step={0.1} />
     </Item>
-    <Button type="primary">Pesquisa</Button>
+    <Item label="Capacidade da bateria" name="battery_power">
+      <Slider min={500} max={5000} />
+    </Item>
+    {[
+      { name: "blue", label: "Bluetooth" },
+      { name: "dual_sim", label: "Dual SIM" },
+      { name: "four_g", label: "4G" },
+      { name: "three_g", label: "3G" },
+      { name: "wifi", label: "Wifi" },
+      { name: "touch_screen", label: "Touch screen" },
+    ].map(({ label, name }, index) => (
+      <Item
+        key={index}
+        name={name}
+        valuePropName="checked"
+        initialValue={false}
+      >
+        <Checkbox>{label}</Checkbox>
+      </Item>
+    ))}
+    <Button htmlType="submit" type="primary">
+      Pesquisa
+    </Button>
   </Form>
 );
