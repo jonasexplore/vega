1678db9aab18c8b6456ee93ad8b921287c7ca0ff
Jonas <jonasexplore@gmail.com>
2022-05-14 12:40:50 -0300

    refactor(pages): send onFinish function to searchForm

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,10 +2,14 @@ import type { NextPage } from "next";
 import { Container, SearchForm } from "../components";
 
 const Home: NextPage = () => {
+  const onFinish = (props: any) => {
+    console.log(props);
+  };
+
   return (
     <Container>
       <h2>Calcular valor do investimento</h2>
-      <SearchForm />
+      <SearchForm onFinish={onFinish} />
     </Container>
   );
 };
