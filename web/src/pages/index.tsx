67ade20991057000d31fec07332e6c4263b9abb0
Jonas <jonasexplore@gmail.com>
2022-05-17 18:39:02 -0300

    feat(pages): added post request and loading state

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,15 +1,32 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import { api } from "../api";
 import { Container, PredictForm } from "../components";
 
 const Home: NextPage = () => {
-  const onFinish = (props: any) => {
-    console.log(props);
+  const [isLoading, setIsLoading] = useState(false);
+  const onFinish = async (props: any) => {
+    setIsLoading(true);
+    Object.keys(props).forEach((key) => {
+      if (typeof props[key] === "boolean") {
+        props[key] = Number(props[key]);
+      }
+    });
+
+    try {
+      const result = await api.post("/predict", props);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <Container>
       <h2>Calcular valor do investimento</h2>
-      <PredictForm onFinish={onFinish} />
+      <PredictForm isLoading={isLoading} onFinish={onFinish} />
     </Container>
   );
 };
