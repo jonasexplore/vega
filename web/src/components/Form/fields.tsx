e6273f017455129799b2d2fc77b15d05fb6023fc
Jonas <jonasexplore@gmail.com>
2022-06-14 21:44:54 -0300

    feat(components): separeted fields and export list

--- web/src/components/Form/fields.tsx
+++ web/src/components/Form/fields.tsx
@@ -11,42 +11,7 @@ export type CheckboxFieldProps = {
   name: string;
 };
 
-export const sliderFields: SliderFieldProps[] = [
-  {
-    label: "Memória interna (GB)",
-    name: "int_memory",
-    min: 16,
-    max: 512,
-    step: 16,
-  },
-  {
-    label: "Quantidade de RAM (MB)",
-    name: "ram",
-    min: 512,
-    max: 16384,
-    step: 512,
-  },
-  {
-    label: "Nº de núcleos",
-    name: "n_cores",
-    min: 1,
-    max: 8,
-    step: 1,
-  },
-  {
-    label: "Clock do processador (Ghz)",
-    name: "clock_speed",
-    min: 0.1,
-    max: 4,
-    step: 0.1,
-  },
-  {
-    label: "Capacidade da bateria",
-    name: "battery_power",
-    min: 500,
-    max: 5000,
-    step: 1,
-  },
+export const screenFields: SliderFieldProps[] = [
   {
     label: "Comprimento da tela (px)",
     name: "px_height",
@@ -61,6 +26,70 @@ export const sliderFields: SliderFieldProps[] = [
     max: 2160,
     step: 16,
   },
+];
+
+export const memoriesFields: SliderFieldProps[] = [
+  {
+    label: "Memória interna (GB)",
+    name: "int_memory",
+    min: 16,
+    max: 512,
+    step: 16,
+  },
+  {
+    label: "Quantidade de RAM (MB)",
+    name: "ram",
+    min: 512,
+    max: 16384,
+    step: 512,
+  },
+];
+
+export const bateryFields: SliderFieldProps[] = [
+  {
+    label: "Capacidade da bateria",
+    name: "battery_power",
+    min: 500,
+    max: 5000,
+    step: 1,
+  },
+  {
+    label: "Tempo de bateria (h)",
+    name: "talk_time",
+    min: 1,
+    max: 72,
+    step: 1,
+  },
+];
+
+export const processorFields: SliderFieldProps[] = [
+  {
+    label: "Nº de núcleos",
+    name: "n_cores",
+    min: 1,
+    max: 8,
+    step: 1,
+  },
+  {
+    label: "Clock do processador (Ghz)",
+    name: "clock_speed",
+    min: 0.1,
+    max: 4,
+    step: 0.1,
+  },
+];
+
+export const cameraFields: SliderFieldProps[] = [
+  {
+    label: "Câmera traseira (MP)",
+    name: "pc",
+    min: 1,
+    max: 120,
+    step: 1,
+  },
+];
+
+export const phisicalFields: SliderFieldProps[] = [
   {
     label: "Comprimento do smartphone (cm)",
     name: "sc_h",
@@ -89,20 +118,6 @@ export const sliderFields: SliderFieldProps[] = [
     max: 1000,
     step: 1,
   },
-  {
-    label: "Câmera traseira (MP)",
-    name: "pc",
-    min: 1,
-    max: 120,
-    step: 1,
-  },
-  {
-    label: "Tempo de bateria (h)",
-    name: "talk_time",
-    min: 1,
-    max: 72,
-    step: 1,
-  },
 ];
 
 export const checkboxFields: CheckboxFieldProps[] = [
@@ -113,4 +128,48 @@ export const checkboxFields: CheckboxFieldProps[] = [
   { name: "wifi", label: "Wifi" },
   { name: "touch_screen", label: "Touch screen" },
   { name: "fc", label: "Câmera frontal" },
+];
+
+type fieldsProps = {
+  header: string;
+  type: string;
+  fields: SliderFieldProps[] | CheckboxFieldProps[];
+};
+
+export const fields: fieldsProps[] = [
+  {
+    header: "Tela",
+    type: "slider",
+    fields: screenFields,
+  },
+  {
+    header: "Memória",
+    type: "slider",
+    fields: memoriesFields,
+  },
+  {
+    header: "Bateria",
+    type: "slider",
+    fields: bateryFields,
+  },
+  {
+    header: "Processador",
+    type: "slider",
+    fields: processorFields,
+  },
+  {
+    header: "Câmera",
+    type: "slider",
+    fields: cameraFields,
+  },
+  {
+    header: "Características Físicas",
+    type: "slider",
+    fields: phisicalFields,
+  },
+  {
+    header: "Outros",
+    type: "checkbox",
+    fields: checkboxFields,
+  },
 ];
