3705a5c87ff3e1a18f05ae646a88b94ea8868e8f
Jonas <jonasexplore@gmail.com>
2022-06-20 21:10:32 -0300

    refactor(components): adapted state and insert input component

--- web/src/components/Form/index.tsx
+++ web/src/components/Form/index.tsx
@@ -1,22 +1,71 @@
-import { Button, Checkbox, Collapse, Form, Slider } from "antd";
+import { Button, Checkbox, Collapse, Form, Input, Slider } from "antd";
+import { useState } from "react";
 import { CheckboxFieldProps, SliderFieldProps, fields } from "./fields";
 
 type Props = {
   isLoading: boolean;
   onFinish: (props: any) => void;
+  onReset: () => void;
 };
 
 const { Item } = Form;
 const { Panel } = Collapse;
 
-export const PredictForm = ({ isLoading, onFinish }: Props) => {
+const inicialValues = {
+  px_height: "",
+  px_width: "",
+  int_memory: "",
+  ram: "",
+  battery_power: "",
+  talk_time: "",
+  n_cores: "",
+  clock_speed: "",
+  pc: "",
+  sc_h: "",
+  sc_w: "",
+  m_dep: "",
+  mobile_wt: "",
+  blue: false,
+  dual_sim: false,
+  four_g: false,
+  three_g: false,
+  wifi: false,
+  touch_screen: false,
+  fc: false,
+} as any;
+
+export const PredictForm = ({ isLoading, onFinish, onReset }: Props) => {
+  const [values, setValues] = useState(inicialValues);
+  const [activeKey, setActiveKey] = useState([] as any);
+
+  const onChange = (value: number | boolean, name: string) => {
+    setValues({ ...values, [name]: value });
+  };
+
   const renderSlideFields = (
     { label, max, min, name, step }: SliderFieldProps,
     index: number
   ) => (
-    <Item key={index} label={label} name={name}>
-      <Slider min={min} max={max} step={step} />
-    </Item>
+    <div>
+      <Item style={{ flex: 1 }} key={index} label={label} name={name} required>
+        <Slider
+          min={min}
+          max={max}
+          step={step}
+          onChange={(value) => onChange(value, name)}
+          value={values[name] ?? ""}
+        />
+        <Input
+          value={values[name] ?? ""}
+          onChange={(event) => onChange(Number(event.target.value), name)}
+          type="number"
+          min={min}
+          max={max}
+          step={step}
+          required
+        />
+      </Item>
+    </div>
   );
 
   const renderCheckboxFields = (
@@ -24,19 +73,27 @@ export const PredictForm = ({ isLoading, onFinish }: Props) => {
     index: number
   ) => (
     <Item key={index} name={name} valuePropName="checked" initialValue={false}>
-      <Checkbox>{label}</Checkbox>
+      <Checkbox
+        onChange={(e) => onChange(e.target.checked, name)}
+        checked={values[name]}
+      >
+        {label}
+      </Checkbox>
     </Item>
   );
 
   return (
     <Form
-      style={{
-        width: "50%",
-        marginBottom: 32,
+      onFinish={() => {
+        onFinish(values);
       }}
-      onFinish={onFinish}
     >
-      <Collapse>
+      <Collapse
+        activeKey={activeKey}
+        onChange={(key) => {
+          setActiveKey(key);
+        }}
+      >
         {fields.map(({ fields, header, type }, index) => (
           <Panel
             header={header}
@@ -60,11 +117,28 @@ export const PredictForm = ({ isLoading, onFinish }: Props) => {
         htmlType="submit"
         type="primary"
         loading={isLoading}
+        onClick={() => setActiveKey(Array.from(Array(fields.length).keys()))}
         style={{
           marginTop: 16,
         }}
       >
         Pesquisa
+      </Button>
+
+      <Button
+        htmlType="reset"
+        type="default"
+        onClick={() => {
+          setActiveKey([]);
+          setValues(inicialValues);
+          onReset();
+        }}
+        style={{
+          marginLeft: 8,
+          marginTop: 16,
+        }}
+      >
+        Limpar
       </Button>
     </Form>
   );
