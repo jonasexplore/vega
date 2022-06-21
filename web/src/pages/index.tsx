5156e9334d185b30f5cf32f225ef45e369ca7153
Jonas <jonasexplore@gmail.com>
2022-06-20 21:11:21 -0300

    fix(pages): fixed response and show result modal

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,11 +2,20 @@ import type { NextPage } from "next";
 import { useState } from "react";
 import { api } from "../api";
 import { Container, PredictForm } from "../components";
+import { CardResult } from "../components/Card";
 
 const Home: NextPage = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState("");
+  const [showResult, setShowResult] = useState(false);
+
+  const onReset = () => {
+    setShowResult(false);
+  };
 
   const onFinish = async (props: any) => {
+    console.log(props);
+
     setIsLoading(true);
     Object.keys(props).forEach((key) => {
       if (typeof props[key] === "boolean") {
@@ -15,8 +24,12 @@ const Home: NextPage = () => {
     });
 
     try {
-      const result = await api.post("/predict", props);
-      console.log(result);
+      const {
+        data: [response],
+      } = await api.post("/predict", props);
+      setResult(response);
+      setShowResult(true);
+      console.log(response);
     } catch (error) {
       console.log(error);
     } finally {
@@ -27,7 +40,19 @@ const Home: NextPage = () => {
   return (
     <Container>
       <h2>Calcular valor do investimento</h2>
-      <PredictForm isLoading={isLoading} onFinish={onFinish} />
+      <div
+        style={{
+          width: "50%",
+          marginBottom: 32,
+        }}
+      >
+        <PredictForm
+          isLoading={isLoading}
+          onFinish={onFinish}
+          onReset={onReset}
+        />
+        {showResult && <CardResult result={result} />}
+      </div>
     </Container>
   );
 };
